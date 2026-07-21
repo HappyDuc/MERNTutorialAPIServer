2be20988fd918df98866a867516a783ec6c9f96a
Jake Sinclair <sinclairs1234@gmail.com>
2026-07-21 22:13:09 +0100

    Absolute idiocy fixed, also removed manual cors middleware.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["process.env.CLIENT_URL"],
+        origin: [process.env.CLIENT_URL],
         credentials: true,
     }),
 );
@@ -19,14 +19,14 @@ app.use(clerkMiddleware());
 app.use("/webhooks", webhookRouter);
 app.use(express.json());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept",
-    );
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept",
+//     );
+//     next();
+// });
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
